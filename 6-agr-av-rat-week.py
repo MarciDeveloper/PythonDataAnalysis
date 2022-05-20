bfa796c63fc480062eff25c82761d41de8d20e5f
Marcello Caccamo <londonliving89@gmail.com>
2022-05-20 13:10:44 +1000

    styling changes

--- 6-agr-av-rat-week.py
+++ 6-agr-av-rat-week.py
@@ -74,7 +74,7 @@ chart_def = """
 def app():
     wp = jp.QuasarPage()
     h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
-    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis", classes="text-h4 text-center q-pa-md")
  
     hc = jp.HighCharts(a=wp, options=chart_def)
     hc.options.xAxis.categories = list(weekday_average.index.get_level_values(0))
